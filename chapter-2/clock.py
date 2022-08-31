553061de4a6ae43f6acb8b4688c44a8804dac140
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-08-31 22:02:04 +0000

    Edit clock.py

---
+++ chapter-2/clock.py
@@ -0,0 +1,8 @@
+now = input(print("what time is it now in hours (1-24): "))
+x=0
+while x=0:
+  if now >= 1 and now <=24:
+    x=1
+    hrs = input(print("In how many hours should the alarm go off: "))
+  else:
+    now = input(print("input invalid, pick an hour from 1-24: "))
