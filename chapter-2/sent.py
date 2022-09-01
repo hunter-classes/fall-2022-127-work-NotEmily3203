240b12e1781f6875d4b211197c6d3ec5b7cfdb9b
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-09-01 01:39:38 +0000

    Edit sent.py

---
+++ chapter-2/sent.py
@@ -0,0 +1,11 @@
+one="All "
+two="work "
+three="and "
+four="no "
+five="play "
+six="makes "
+seven="Jack "
+eight="a "
+nine="dull "
+ten="boy."
+print(one,two,three,four,five,six,seven,eight,nine,ten)
