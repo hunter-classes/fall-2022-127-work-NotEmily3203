d8ce7bc6541d4bb4bc97dad178e7fe3ee3fc436f
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-09-01 12:58:23 +0000

    Created turtle folder and square.py

---
+++ turtles/square.py
@@ -0,0 +1,17 @@
+import turtle
+
+wn = turtle.Screen()
+
+crush = turtle.Turtle()
+
+crush.forward(50)
+crush.left(90)
+crush.forward(50)
+crush.left(90)
+crush.forward(50)
+crush.left(90)
+crush.forward(50)
+crush.left(90)
+
+wn.exitonclick()
+wn.mainloop()
