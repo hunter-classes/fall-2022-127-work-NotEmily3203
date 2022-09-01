dff38b22f99fb9f6042082bea3e47d96cd59227b
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-09-01 13:38:45 +0000

    edited more turtle stuff

--- chapter-2/circ.py
+++ chapter-2/circ.py
@@ -1,3 +1,3 @@
 radius = float(input("what is your radius: "))
-volume = 3.14159 * radius * radius
-print(f"Your circle's volume is {volume}")
+area = 3.14159 * radius * radius
+print(f"Your circle's area is {area}")
