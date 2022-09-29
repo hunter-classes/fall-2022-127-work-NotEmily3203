7ba88e45bbc15dea9facf384028f57ca0295176f
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-09-29 12:41:14 +0000

    fizzbuzz.py revision

--- piglatin/fizzbuzz.py
+++ piglatin/fizzbuzz.py
@@ -1,9 +1,12 @@
-for i in range(1,101):
-  if i % 5 == 0 and i % 3 == 0:
-    print("fizzbuzz")
-  elif i % 5 == 0:
-    print("buzz")
-  elif i % 3 == 0:
-    print("fizz")
-  else:
-    print(i)
+def fizzbuzz(n):
+  for i in range(1,n+1):
+    if i % 5 == 0 and i % 3 == 0:
+      print("fizzbuzz")
+    elif i % 5 == 0:
+      print("buzz")
+    elif i % 3 == 0:
+      print("fizz")
+    else:
+      print(i)
+
+fizzbuzz(22)
