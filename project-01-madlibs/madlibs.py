d6216666da83612ecfc3ef93617308d6644388db
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-10-22 22:01:18 +0000

    added to madlib

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -2,14 +2,13 @@
 EXTRAS:
 -Instead of specifying the sentences or story to convert, write a story in a file and read it from your program. Make sure to include the file in your repo and that your program reads it correctly.
 -Add some replacements that are unique, that is, the first time you see them you select on randomly but then you keep reusing that replacement.
--Pay attention to letter case. That is, if you replace a word at the beginning of a sentence, it should be capitalized, otherwise, lowercase. This is except in the case of proper nouns which should always be capitalized.
 """
 
 import random
 
-villain = ["Radioactive Cockroch","Mutant Rat","Poisonous Lanternfly","Poopy Pigeon","Super-Crackhead","Hawky 🦅","Florida Man"]
-aggroverb = ["run","dance","punch","kick","jump","kiss","flirt","grab"]
-weapon = ["pistol","katana","stick","improvised toilet stall","cigarette butt"]
+villain = ["radioactive cockroch","mutant rat","poisonous lanternfly","poopy pigeon","super-crackhead","Hawky 🦅","Florida man"]
+aggroverb = ["dance with","punch","kick","jump on","kiss","flirt with","grab","slap","headbutt"]
+weapon = ["pistol","katana","stick","book","cigarette butt"]
 feelings = ["confused","happy","annoyed","angry","flustered","turned on","digusted","flabbergasted","bamboozled","in Ohio"]
 
 randvillain = random.choice(villain)
