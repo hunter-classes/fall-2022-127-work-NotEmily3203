ff2a6bec6f04760c2cb63ff013ce09acb132a86a
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-11-27 03:20:07 +0000

    all done with pirate wahooo

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,7 +1,9 @@
 """
 EXTRAS:
 -Store your translations in a file named pirate.dat the file should have lines in the form “word:translation.”
-- Handle upper and lower case and punctuation
+-Handle upper and lower case and punctuation
+-Have an option to translate different languages (Pirate and Austro)
+-Converting parts of words rather than straight substitutions
 """
 #puts translation into dict
 pirate = {}
@@ -17,23 +19,54 @@ klist = [x for x in pirate.keys()]
 s = open("input.txt")
 story = s.read()
 words = story.split(" ")
-print(words)
 for i in words:
   x = 0
   if i.isalpha() == False:
-    e = i[0:-1]
+    e = i[0:-1].lower()
     p = i[-1:]
     x = 1
   else:
-    e = i
+    e = i.lower()
   if e in klist:
     index = words.index(i)
-    if i == i.capitalize(): #checks if word is capitalized
+    if i == e.capitalize(): #checks if word is capitalized
       words[index] = i.replace(i,pirate[e].capitalize())
     else:
       words[index] = i.replace(i,pirate[e])
     if x == 1:
       words[index] += p
-translate = " ".join(words)
-print("before: ",story)
-print("after: ",translate)
+ptranslate = " ".join(words)
+
+austro = {}
+f = open("austro.dat")
+lines = f.read()
+perline = lines.split("\n")
+for i in perline:
+  index = i.rfind(":")
+  austro[i[0:index]]=i[index+1:]
+
+klist = [x for x in austro.keys()]
+
+s = open("input.txt")
+story = s.read()
+words = story.split(" ")
+for i in words:
+  x = 0
+  if i.isalpha() == False:
+    e = i[0:-1].lower()
+    p = i[-1:]
+    x = 1
+  else:
+    e = i.lower()
+  for l in klist:
+    if l in e:
+      windex = words.index(i)
+      lindex = i.index(l)
+      words[windex] = i.replace(i[lindex:lindex+len(l)],austro[l])
+      if x == 1:
+        words[index] += p
+atranslate = " ".join(words)
+
+print("ORIGINAL: ", story,"\n")
+print("PIRATE: ",ptranslate,"\n")
+print("AUSTRO: ",atranslate,"\n")
