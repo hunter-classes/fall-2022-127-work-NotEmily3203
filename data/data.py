40321582f65fbcd61ea51595d80ef27c9b3cb196
NotEmily <90732072+NotEmily3203@users.noreply.github.com>
2022-12-12 02:14:29 +0000

    added data.py and csv files

---
+++ data/data.py
@@ -0,0 +1,12 @@
+"""
+EXTRAS:
+- Use Matplotlib or another Python graphing/plotting library to visualize part or all of your analysis
+- Use more than one data source and have your analysis compare,contrast, or correlate them
+- Use multiple aspects of a single data source in your analysis
+"""
+
+#which is healthier, starbucks or cereal??
+
+#read the data
+c = open("cereal.csv")
+s = open("starbucks-menu-nutrition-drinks.csv")
